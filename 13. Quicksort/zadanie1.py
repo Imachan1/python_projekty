d1ac6000f425aef2bec62238c7c7f787ac8a6667
Valeriia <139450358+Imachan1@users.noreply.github.com>
2025-03-19 15:07:46 +0100

    Create zadanie1.py

---
+++ 13. Quicksort/zadanie1.py
@@ -0,0 +1,19 @@
+def quicksort(ciag,lewy,prawy):
+    i=lewy
+    j=prawy
+    pivot = ciag[(i+j)/2]
+    while i<=j:
+        while ciag[i]<pivot:
+            i=+1
+        while ciag[j]>pivot:
+            j=-1
+        if i<=j:
+            pom=ciag[i]
+            ciag[i]=ciag[j]
+            ciag[j]=pom
+            i=+1
+            j=-1
+    if j<lewy:
+        quicksort(ciag,lewy,i)
+    if i<prawy:
+        quicksort(ciag,i,prawy)
