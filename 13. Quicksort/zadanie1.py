e56b0d82298cd391bd37e2935798e9f1a28eb101
Valeriia <139450358+Imachan1@users.noreply.github.com>
2025-03-20 07:44:17 +0100

    Update zadanie1.py

--- 13. Quicksort/zadanie1.py
+++ 13. Quicksort/zadanie1.py
@@ -1,19 +1,25 @@
 def quicksort(ciag,lewy,prawy):
     i=lewy
     j=prawy
-    pivot = ciag[(i+j)/2]
+    pivot = ciag[(i+j)//2]
     while i<=j:
         while ciag[i]<pivot:
-            i=+1
+            i+=1
         while ciag[j]>pivot:
-            j=-1
+            j-=1
         if i<=j:
             pom=ciag[i]
             ciag[i]=ciag[j]
             ciag[j]=pom
-            i=+1
-            j=-1
-    if j<lewy:
-        quicksort(ciag,lewy,i)
+            i+=1
+            j-=1
+    if j>lewy:
+        quicksort(ciag,lewy,j)
     if i<prawy:
         quicksort(ciag,i,prawy)
+
+with open("liczby.txt","r") as file:
+    c=list(map(int,file.readlines()))
+
+quicksort(c,0,len(c)-1)
+print(c)
